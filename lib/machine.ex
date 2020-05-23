65ef323f2e9e6839fc2b84fb01102f9ac23c8db2
Benjamin Manford <benjaminmanford@gmail.com>
2020-05-23 15:32:50 +0800

    Update Machine

--- lib/machine.ex
+++ lib/machine.ex
@@ -4,16 +4,34 @@ defmodule Machine do
   """
 
   @doc """
-  Ussd.machine.setRequest()
+  Ussd.machine.set()
 
   ## Examples
-
-      iex> Ussd.machine.setRequest(%{msisdn: "233544909356", network: "mtn", session_id: "12345678", input: "*123"})
-      "233544909356mtn12345678*123"
-
+      iex> Ussd.machine.set(%{msisdn: "233544909356", network: "mtn", session_id: "12345678", input: "*123"})
+      %{action: 'input', message: 'Hello world!!'}
   """
 
-  def setRequest(%{msisdn: msisdn, network: network, session_id: session_id, input: input}) do
-    msisdn <> network <> session_id <> input
+  defstruct [:msisdn, :network, :session_id, :input]
+  @session_id_exception 'session_id needs to be set before ussd machine can run.'
+
+  def set(request = %{msisdn: msisdn, network: network, session_id: session_id, input: input}) do
+    request |> run
+  end
+
+  def run(request) do
+    case request[:session_id] do
+      nil -> raise @session_id_exception
+      ""  -> raise @session_id_exception
+      _   -> true
+    end
+
+    response('Hello world!!', 'input')
+  end
+
+  def response(message, action) do
+    %{
+      message: message,
+      action: action
+    }
   end
 end
